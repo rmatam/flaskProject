c9c9ff1bab6c230b7bf6fb4fe8f475c39638804f
rmatam <sekhar.matam@gmail.com>
2022-08-03 22:03:34 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/index')
+def index():  # put application's code here
+    return 'Welcome to the course!'
+
+
+if __name__ == '__main__':
+    app.run()
